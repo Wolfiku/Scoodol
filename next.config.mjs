6d7556e65ed157c0dac04306fc41d1fdf136795c
Wolfgang Kupfer <wolfiku90@gmail.com>
2025-09-22 11:27:03 +0000

    Ok önnen wir das ganze mit manifesst und service worker so umbauen das e

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
-import type {NextConfig} from 'next';
+/** @type {import('next').NextConfig} */
+import withPWA from '@ducanh2912/next-pwa';
 
-const nextConfig: NextConfig = {
+const pwa = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -32,4 +40,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default pwa(nextConfig);
