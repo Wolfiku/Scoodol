9ed8bf1710d8ceafaf91458050597d4666c2a92c
Wolfgang Kupfer <wolfiku90@gmail.com>
2025-09-23 11:33:40 +0000

    Nein auf 1.1.3

--- src/app/components/settings-view.tsx
+++ src/app/components/settings-view.tsx
@@ -398,7 +398,7 @@ export default function SettingsView({ onEditTimetable, isPreview = false, onTim
                     made by @wolfiku, powered by limbo
                 </span>
                 <span>
-                    Version 1.1.2
+                    Version 1.1.3
                 </span>
             </div>
         </div>
