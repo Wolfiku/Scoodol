415b88a4696cbfbcafcd7685de0067282a0152ad
Wolfgang Kupfer <wolfiku90@gmail.com>
2025-10-13 16:41:53 +0000

    Ok nun grösse durch 2

--- src/app/components/setup-view.tsx
+++ src/app/components/setup-view.tsx
@@ -278,7 +278,7 @@ export default function SetupView({ onSetupComplete, onTimetableImport, isEditin
                                     type="time" 
                                     value={timetableSettings.schoolStartTime} 
                                     onChange={e => setTimetableSettings(prev => ({ ...prev, schoolStartTime: e.target.value }))} 
-                                    className="w-full mt-1 text-5xl h-24"
+                                    className="w-full mt-1 text-3xl h-20"
                                 />
                             </div>
                             <div>
@@ -288,7 +288,7 @@ export default function SetupView({ onSetupComplete, onTimetableImport, isEditin
                                     type="time" 
                                     value={timetableSettings.schoolEndTime} 
                                     onChange={e => setTimetableSettings(prev => ({ ...prev, schoolEndTime: e.target.value }))} 
-                                    className="w-full mt-1 text-5xl h-24"
+                                    className="w-full mt-1 text-3xl h-20"
                                 />
                             </div>
                         </div>
@@ -403,5 +403,7 @@ export default function SetupView({ onSetupComplete, onTimetableImport, isEditin
 
 
 
+
+    
 
     
